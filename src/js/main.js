a7c808b193cf779a4e05dd6e85fc720203e3c18c
Marcos <marcoswodbarros@outlook.com>
2023-03-28 09:20:44 -0300

    Starting the js

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+let email = document.querySelector("#email");
+let password = document.querySelector("#password");
+
+function emailYellow () {
+    email.style.borderColor = "#ffc632";
+}
+
+function emailNormal () {
+    email.style.borderColor = "#afb6c2";
+}
+
+
+function passwordYellow () {
+    password.style.borderColor = "#ffc632";
+}
+
+function passwordNormal () {
+    password.style.borderColor = "#afb6c2";
+}
