7f0e7dad81fca6bcbaab8136b0e6e56008194bfc
Marcos <marcoswodbarros@outlook.com>
2023-03-28 09:28:39 -0300

    Updating js

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,31 @@
 let email = document.querySelector("#email");
+let mailImg = document.querySelector("#mail");
+let mailImgOn = document.querySelector("#mail__on");
 let password = document.querySelector("#password");
+let passwordImg = document.querySelector("#password__img");
+let passwordImgOn = document.querySelector("#password__img--on");
 
 function emailYellow () {
     email.style.borderColor = "#ffc632";
+    mailImg.style.display = "none";
+    mailImgOn.style.display = "block";
 }
 
 function emailNormal () {
     email.style.borderColor = "#afb6c2";
+    mailImg.style.display = "block";
+    mailImgOn.style.display = "none";
 }
 
 
 function passwordYellow () {
     password.style.borderColor = "#ffc632";
+    passwordImg.style.display = "none";
+    passwordImgOn.style.display = "block";
 }
 
 function passwordNormal () {
     password.style.borderColor = "#afb6c2";
+    passwordImg.style.display = "block";
+    passwordImgOn.style.display = "none";
 }
